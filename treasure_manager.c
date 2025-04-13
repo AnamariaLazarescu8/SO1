b84a308971207a70b3ebe227b89129a223579801
AnamariaLazarescu8 <anamaria.lazarescu@student.upt.ro>
2025-04-13 23:10:18 +0300

    Add files via upload

--- treasure_manager.c
+++ treasure_manager.c
@@ -5,7 +5,9 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <dirent.h>
 #include <time.h>
+#include <errno.h>
 
 #define MAX_NAME_LEN 100
 #define MAX_CLUE_LEN 255
@@ -19,23 +21,50 @@ typedef struct {
     int value;
 } Treasure;
 
-//Crearea unui director pentru joc
 int create_hunt_directory(const char* hunt_id) {
     char path[256];
     snprintf(path, sizeof(path), "./%s", hunt_id);
-    if (mkdir(path, 0755) == -1) {
+    if (mkdir(path, 0755) == -1 && errno != EEXIST) {
         perror("Error creating hunt directory");
         return -1;
     }
     return 0;
 }
 
+void log_operation(const char* hunt_id, const char* operation) {
+    char log_filename[256];
+    snprintf(log_filename, sizeof(log_filename), "./%s/logged_hunt", hunt_id);
 
-//adaugarea unei comori intr-un fisier
+    FILE *logfile = fopen(log_filename, "a");
+    if (!logfile) {
+        perror("Error opening log file");
+        return;
+    }
+
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    fprintf(logfile, "[%04d-%02d-%02d %02d:%02d:%02d] %s\n",
+        tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
+        tm.tm_hour, tm.tm_min, tm.tm_sec, operation);
+    fclose(logfile);
+}
+
+int create_symlink_for_log(const char* hunt_id) {
+    char log_filename[256], symlink_name[256];
+    snprintf(log_filename, sizeof(log_filename), "./%s/logged_hunt", hunt_id);
+    snprintf(symlink_name, sizeof(symlink_name), "./logged_hunt-%s", hunt_id);
+
+    unlink(symlink_name); // remove old symlink if exists
+    if (symlink(log_filename, symlink_name) == -1) {
+        perror("Error creating symlink");
+        return -1;
+    }
+    return 0;
+}
+
 int add_treasure(const char* hunt_id, Treasure* treasure) {
     char filename[256];
     snprintf(filename, sizeof(filename), "./%s/treasures.dat", hunt_id);
-
     int fd = open(filename, O_RDWR | O_CREAT | O_APPEND, 0644);
     if (fd == -1) {
         perror("Error opening treasure file");
@@ -43,19 +72,29 @@ int add_treasure(const char* hunt_id, Treasure* treasure) {
     }
 
     if (write(fd, treasure, sizeof(Treasure)) != sizeof(Treasure)) {
-        perror("Error writing treasure to file");
+        perror("Error writing treasure");
         close(fd);
         return -1;
     }
 
     close(fd);
+
+    log_operation(hunt_id, "add treasure");
+    create_symlink_for_log(hunt_id);
     return 0;
 }
 
-//listarea comorilor dintr-un joc
 int list_treasures(const char* hunt_id) {
     char filename[256];
     snprintf(filename, sizeof(filename), "./%s/treasures.dat", hunt_id);
+    struct stat st;
+    if (stat(filename, &st) == -1) {
+        perror("Error stating treasure file");
+        return -1;
+    }
+
+    printf("Hunt: %s\nFile size: %ld bytes\nLast modified: %s",
+           hunt_id, st.st_size, ctime(&st.st_mtime));
 
     int fd = open(filename, O_RDONLY);
     if (fd == -1) {
@@ -65,20 +104,20 @@ int list_treasures(const char* hunt_id) {
 
     Treasure treasure;
     while (read(fd, &treasure, sizeof(Treasure)) > 0) {
-        printf("ID: %d, User: %s, Coordinates: (%.2f, %.2f), Clue: %s, Value: %d\n",
-               treasure.treasure_id, treasure.username, treasure.latitude,
-               treasure.longitude, treasure.clue, treasure.value);
+        printf("ID: %d, User: %s, Coords: (%.2f, %.2f), Clue: %s, Value: %d\n",
+               treasure.treasure_id, treasure.username,
+               treasure.latitude, treasure.longitude,
+               treasure.clue, treasure.value);
     }
 
     close(fd);
+    log_operation(hunt_id, "list treasures");
     return 0;
 }
 
-//vizualizarea unei comori
 int view_treasure(const char* hunt_id, int treasure_id) {
     char filename[256];
     snprintf(filename, sizeof(filename), "./%s/treasures.dat", hunt_id);
-
     int fd = open(filename, O_RDONLY);
     if (fd == -1) {
         perror("Error opening treasure file");
@@ -88,10 +127,12 @@ int view_treasure(const char* hunt_id, int treasure_id) {
     Treasure treasure;
     while (read(fd, &treasure, sizeof(Treasure)) > 0) {
         if (treasure.treasure_id == treasure_id) {
-            printf("ID: %d, User: %s, Coordinates: (%.2f, %.2f), Clue: %s, Value: %d\n",
-                   treasure.treasure_id, treasure.username, treasure.latitude,
-                   treasure.longitude, treasure.clue, treasure.value);
+            printf("ID: %d, User: %s, Coords: (%.2f, %.2f), Clue: %s, Value: %d\n",
+                   treasure.treasure_id, treasure.username,
+                   treasure.latitude, treasure.longitude,
+                   treasure.clue, treasure.value);
             close(fd);
+            log_operation(hunt_id, "view treasure");
             return 0;
         }
     }
@@ -101,77 +142,77 @@ int view_treasure(const char* hunt_id, int treasure_id) {
     return -1;
 }
 
-//stergerea unei comori 
 int remove_treasure(const char* hunt_id, int treasure_id) {
     char filename[256];
     snprintf(filename, sizeof(filename), "./%s/treasures.dat", hunt_id);
 
-    int fd = open(filename, O_RDWR);
+    int fd = open(filename, O_RDONLY);
     if (fd == -1) {
         perror("Error opening treasure file");
         return -1;
     }
 
     Treasure treasure;
-    int pos = 0;
-    int found = 0;
+    Treasure* all = NULL;
+    int count = 0;
+
     while (read(fd, &treasure, sizeof(Treasure)) > 0) {
-        if (treasure.treasure_id == treasure_id) {
-            found = 1;
-            break;
+        if (treasure.treasure_id != treasure_id) {
+            all = realloc(all, sizeof(Treasure) * (count + 1));
+            all[count++] = treasure;
         }
-        pos++;
+    }
+    close(fd);
+
+    fd = open(filename, O_WRONLY | O_TRUNC);
+    if (fd == -1) {
+        perror("Error truncating file");
+        free(all);
+        return -1;
     }
 
-    if (!found) {
-        printf("Treasure not found.\n");
-        close(fd);
-        return -1;
+    if (write(fd, all, sizeof(Treasure) * count) == -1) {
+        perror("Error writing back data");
     }
 
     close(fd);
+    free(all);
+
+    log_operation(hunt_id, "remove treasure");
     return 0;
 }
 
+int remove_hunt(const char* hunt_id) {
+    char path[256];
+    snprintf(path, sizeof(path), "./%s", hunt_id);
 
-
-//logarea operatiunilor
-void log_operation(const char* hunt_id, const char* operation) {
-    char log_filename[256];
-    snprintf(log_filename, sizeof(log_filename), "./%s/logged_hunt", hunt_id);
-
-    FILE *logfile = fopen(log_filename, "a");
-    if (logfile == NULL) {
-        perror("Error opening log file");
-        return;
-    }
-
-    time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
-    fprintf(logfile, "[%d-%02d-%02d %02d:%02d:%02d] %s\n",
-            tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
-            tm.tm_hour, tm.tm_min, tm.tm_sec, operation);
-
-    fclose(logfile);
-}
-
-//crearea legaturii simbolice pentru loguri
-int create_symlink_for_log(const char* hunt_id) {
-    char log_filename[256];
-    snprintf(log_filename, sizeof(log_filename), "./%s/logged_hunt", hunt_id);
-
-    char symlink_name[256];
-    snprintf(symlink_name, sizeof(symlink_name), "./logged_hunt-%s", hunt_id);
-
-    if (symlink(log_filename, symlink_name) == -1) {
-        perror("Error creating symlink");
+    DIR *dir = opendir(path);
+    if (!dir) {
+        perror("Cannot open hunt directory");
         return -1;
     }
 
+    struct dirent *entry;
+    char filepath[512];
+    while ((entry = readdir(dir)) != NULL) {
+        if (!strcmp(entry->d_name, ".") || !strcmp(entry->d_name, "..")) continue;
+        snprintf(filepath, sizeof(filepath), "%s/%s", path, entry->d_name);
+        if (unlink(filepath) == -1) perror("Error deleting file");
+    }
+    closedir(dir);
+
+    if (rmdir(path) == -1) {
+        perror("Failed to delete hunt directory");
+        return -1;
+    }
+
+    char symlink_name[256];
+    snprintf(symlink_name, sizeof(symlink_name), "./logged_hunt-%s", hunt_id);
+    unlink(symlink_name);
+
+    printf("Hunt %s deleted.\n", hunt_id);
     return 0;
 }
-
-
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
@@ -183,25 +224,56 @@ int main(int argc, char *argv[]) {
     const char *hunt_id = argv[2];
 
     if (strcmp(command, "add") == 0) {
-        if (argc != 8) {
+        if (argc != 9) {
             printf("Usage: add <hunt_id> <treasure_id> <username> <latitude> <longitude> <clue> <value>\n");
             return -1;
         }
+        Treasure t;
+        t.treasure_id = atoi(argv[3]);
+        strncpy(t.username, argv[4], MAX_NAME_LEN);
+        t.latitude = atof(argv[5]);
+        t.longitude = atof(argv[6]);
+        strncpy(t.clue, argv[7], MAX_CLUE_LEN);
+        t.value = atoi(argv[8]);
 
-        Treasure treasure;
-        treasure.treasure_id = atoi(argv[3]);
-        strncpy(treasure.username, argv[4], MAX_NAME_LEN);
-        treasure.latitude = atof(argv[5]);
-        treasure.longitude = atof(argv[6]);
-        strncpy(treasure.clue, argv[7], MAX_CLUE_LEN);
-        treasure.value = atoi(argv[8]);
+        create_hunt_directory(hunt_id);
+        return add_treasure(hunt_id, &t);
 
-        add_treasure(hunt_id, &treasure);
+    } else if (strcmp(command, "list") == 0) {
+        return list_treasures(hunt_id);
+
+    } else if (strcmp(command, "view") == 0) {
+        if (argc != 4) {
+            printf("Usage: view <hunt_id> <treasure_id>\n");
+            return -1;
+        }
+        return view_treasure(hunt_id, atoi(argv[3]));
+
+    } else if (strcmp(command, "remove_treasure") == 0) {
+        if (argc != 4) {
+            printf("Usage: remove_treasure <hunt_id> <treasure_id>\n");
+            return -1;
+        }
+        return remove_treasure(hunt_id, atoi(argv[3]));
+
+    } else if (strcmp(command, "remove_hunt") == 0) {
+        return remove_hunt(hunt_id);
+
+    } else {
+        printf("Unknown command.\n");
+        return -1;
     }
-   
-
-    return 0;
 }
 
+/*   
+  gcc -o treasure_manager treasure_manager.c
+./treasure_manager add hunt1 101 user1 45.12 21.43 "Near the oak tree" 100
+./treasure_manager list hunt1
+./treasure_manager view hunt1 101
+./treasure_manager remove_treasure hunt1 101
+./treasure_manager remove_hunt hunt1
 
 
+
+
+*/
